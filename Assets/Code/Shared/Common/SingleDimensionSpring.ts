c1faf9ddfb47a9927f0db28bd523a96703d44ae2
nadia8666 <115858781+nadia8666@users.noreply.github.com>
2025-12-05 19:03:52 -0800

    fix dampening

--- Assets/Code/Shared/Common/SingleDimensionSpring.ts
+++ Assets/Code/Shared/Common/SingleDimensionSpring.ts
@@ -5,7 +5,7 @@
 export class SingleDimensionSpring {
     constructor(public CurrentValue: number = 0,
         public TargetValue: number = 0,
-        public Velocity: number = 0, public MagnitudeDivider: number = 95, public Strength: number = 1, public Dampening: number = 1, public IsAngles: boolean = false) {
+        public Velocity: number = 0, public MagnitudeDivider: number = 95, public Strength: number = 1, public Dampening: number = .97, public IsAngles: boolean = false) {
     }
 
     public Update(DeltaTime: number, TargetValue: number, Set: boolean = false) {
@@ -21,7 +21,7 @@ export class SingleDimensionSpring {
         const Direction = this.IsAngles ? (Diff + 540) % 360 - 180 : Diff
         const Magnitude = math.clamp01(math.abs(Direction) / this.MagnitudeDivider)
         this.Velocity += (this.Strength * Direction * Magnitude) * DeltaTime
-        this.Velocity *= .97
+        this.Velocity *= this.Dampening
         this.CurrentValue += this.Velocity
     }
 }
